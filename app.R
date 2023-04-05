a11ccd401a06bb302540bcb1e155f1005f6e3365
Pozdniakov <bucherr@yandex.ru>
2023-04-05 23:15:32 +0400

    add time dynamics

--- app.R
+++ app.R
@@ -6,11 +6,13 @@ library(rentrez)
 
 # Define Shiny app UI
 ui <- fluidPage(
-  titlePanel("Number of Articles by Term in PubMed"),
+  titlePanel("Number of Articles by Term in PubMed by Year"),
   sidebarLayout(
     sidebarPanel(
       helpText("Enter the terms you'd like to search for in PubMed, separated by commas."),
       textInput("terms", "Search Terms", ""),
+      numericInput("start_year", "Start Year", 2000, min = 1900, max = 2100, step = 1),
+      numericInput("end_year", "End Year", 2020, min = 1900, max = 2100, step = 1),
       actionButton("submit", "Submit")
     ),
     mainPanel(
@@ -22,27 +24,31 @@ ui <- fluidPage(
 # Define Shiny app server
 server <- function(input, output) {
   article_counts <- eventReactive(input$submit, {
-    req(input$terms)
+    req(input$terms, input$start_year, input$end_year)
     
     terms <- strsplit(input$terms, ",")[[1]]
     terms <- str_trim(terms)
     
-    counts <- map_int(terms, function(term) {
-      es <- entrez_search(db = "pubmed", term = term, retmax = 0)
-      es$count
+    years <- input$start_year:input$end_year
+    
+    counts <- map(terms, function(term) {
+      map_int(years, function(year) {
+        es <- entrez_search(db = "pubmed", term = paste0(term, " AND ", year, "[PDAT]"), retmax = 0)
+        es$count
+      })
     })
     
-    tibble(term = terms, count = counts)
+    tibble(term = rep(terms, each = length(years)), year = rep(years, times = length(terms)), count = unlist(counts))
   })
   
   output$articlePlot <- renderPlotly({
     req(article_counts())
     
     plot <- article_counts() %>%
-      ggplot(aes(x = term, y = count, fill = term)) +
-      geom_bar(stat = "identity") +
-      labs(title = "Number of Articles by Term in PubMed",
-           x = "Term",
+      ggplot(aes(x = year, y = count, fill = term)) +
+      geom_bar(stat = "identity", position = "dodge") +
+      labs(title = "Number of Articles by Term in PubMed by Year",
+           x = "Year",
            y = "Number of Articles") +
       theme_minimal()
     ggplotly(plot)
